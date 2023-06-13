d1183e352a4e063ecdddb1171dde89653a92208e
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-06-13 16:32:42 -0400

    Tip Calculator

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get("/greeting/:name", (req, res)=>{
     res.send(`Wow! Hello there, ${givenName} `)
 })
 
+app.get("/tip/:total/:tipPercentage", (req, res)=>{
+  const tota = req.params.total
+  const tipPer = req.params.tipPercentage
+  const totalTips = (tipPer/tota)*100
+  res.send(`The tip of ${tota} dollar with ${tipPer} percentage tip is $${totalTips}`)
+
+})
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
